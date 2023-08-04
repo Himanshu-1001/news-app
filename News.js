06aeb7c6e3eaed1c49b866574287a1df7f5944af
Himanshu-1001 <140977593+Himanshu-1001@users.noreply.github.com>
2023-08-04 23:40:08 +0530

    Add files via upload

--- News.js
+++ News.js
@@ -2,56 +2,63 @@ import React, { Component } from 'react'
 import NewsItems from './NewsItems'
 
 export default class News extends Component {
-  articles=[
-    {
-        "source": {
-            "id": "espn-cric-info",
-            "name": "ESPN Cric Info"
-        },
-        "author": null,
-        "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-        "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-        "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-        "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-        "publishedAt": "2020-04-27T11:41:47Z",
-        "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-    },
-    {
-        "source": {
-            "id": "espn-cric-info",
-            "name": "ESPN Cric Info"
-        },
-        "author": null,
-        "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-        "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-        "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-        "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-        "publishedAt": "2020-03-30T15:26:05Z",
-        "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-    }
-]
   constructor(){
     super();
     this.state={
-      articles: this.articles
+      articles: [],
+      page: 1,
+      totalResults: 0
     }
   }
+
+  async componentDidMount(){
+    let url=`https://newsapi.org/v2/top-headlines?country=us&apiKey=e9b807757ab9462694c3879b8624d3d0&page=${this.state.page}`;
+    let data=await fetch(url);
+    let parsedData=await data.json();
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+    })
+  }
+
+  getPrevious= async ()=>{
+    this.state.page--;
+    let url=`https://newsapi.org/v2/top-headlines?country=us&apiKey=e9b807757ab9462694c3879b8624d3d0&page=${this.state.page}`;
+    let data=await fetch(url);
+    let parsedData=await data.json();
+    this.setState({
+      articles: parsedData.articles
+    })
+
+  }
+
+  getNext= async ()=>{
+    this.state.page++;
+    let url=`https://newsapi.org/v2/top-headlines?country=us&apiKey=e9b807757ab9462694c3879b8624d3d0&page=${this.state.page}`;
+    let data=await fetch(url);
+    let parsedData=await data.json();
+    this.setState({
+      articles: parsedData.articles
+    })
+  }
+
   render() {
     return (
       <div className='my-2'>
         <h1>NewsApp - Top Headlines</h1>
         <div className="row">
-          <div className="col-md-4">
-            <NewsItems title="myTitle" description="myDesc" imageUrl="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg"/>
-          </div>
-          <div className="col-md-4">
-            <NewsItems title="myTitle" description="myDesc" imageUrl="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg"/>
-          </div>
-          <div className="col-md-4">
-            <NewsItems title="myTitle" description="myDesc" imageUrl="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg"/>
-          </div>
+          {this.state.articles.map((element)=>{
+            return <div className="col-md-4">
+              <NewsItems title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url}/>
+            </div>
+          })}
+        </div>
+        <div class="d-flex justify-content-between my-2">
+          <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.getPrevious}>&larr;Previous</button>
+          <button disabled={this.state.page>=Math.ceil(this.state.totalResults/20)}type="button" className="btn btn-dark" onClick={this.getNext}>Next&rarr;</button>
         </div>
       </div>
     )
   }
+
 }
